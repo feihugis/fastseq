8751748eb7de8353a365a7ff6afd2e68d3b111f7
Fei Hu <hufei68@gmail.com>
2020-09-25 23:44:47 +0000

    Update log formatter

--- fastseq/config.py
+++ fastseq/config.py
@@ -11,4 +11,4 @@ FASTSEQ_LOG_LEVEL = os.getenv('FASTSEQ_LOG_LEVEL', FASTSEQ_DEFAULT_LOG_LEVEL)
 FASTSEQ_CACHE_DIR = os.getenv('FASTSEQ_CACHE_DIR', os.path.join(os.sep, 'tmp'))
 
 FASTSEQ_LOG_FORMAT = (
-    '%(levelname)s %(asctime)s %(filename)s:%(lineno)d] %(message)s')
+    '%(levelname)s %(asctime)s %(filename)s::%(funcName)s::%(lineno)d] %(message)s')
